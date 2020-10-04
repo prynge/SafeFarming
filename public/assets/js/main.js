96dac4b41c1c73c5bcf0ea6d93cb9a9730886724
prynge <37028634+prynge@users.noreply.github.com>
2020-10-04 14:32:50 +0100

    push

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,20 +1,3 @@
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
-  navigator.serviceWorker.register('/assets/js/sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-
-    swRegistration = swReg;
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
-  });
-} else {
-  console.warn('Push messaging is not supported');
-  pushButton.textContent = 'Push Not Supported';
-}
-
 (function($) {
 
 	var	$window = $(window),
